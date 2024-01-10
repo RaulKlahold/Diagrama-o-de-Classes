ced1b52f335b08c21bc7f3764ab47def6b97b2ff
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-09 22:28:03 -0300

    tentei Arrumar o JRe

--- DiagramacaoDeClasses/src/com/github/raulklahold/diagramacaodeclasse/NavegadorDeInternet.java
+++ DiagramacaoDeClasses/src/com/github/raulklahold/diagramacaodeclasse/NavegadorDeInternet.java
@@ -1,5 +1,5 @@
 package com.github.raulklahold.diagramacaodeclasse;
 
-public interface interfaceacao {
+public class NavegadorDeInternet {
 
 }
---
+++ DiagramacaoDeClasses/src/com/github/raulklahold/diagramacaodeclasse/RetornaMain.java
@@ -0,0 +1,10 @@
+package com.github.raulklahold.diagramacaodeclasse;
+
+public class RetornaMain {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
