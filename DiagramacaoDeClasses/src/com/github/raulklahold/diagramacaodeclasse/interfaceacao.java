0426b705a55eccb24966dab5b4db1939dcb91647
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-09 22:35:40 -0300

    adicionei as interfaces

--- DiagramacaoDeClasses/src/com/github/raulklahold/diagramacaodeclasse/interfaceacao.java
+++ DiagramacaoDeClasses/src/com/github/raulklahold/diagramacaodeclasse/interfaceacao.java
@@ -1,5 +1,15 @@
 package com.github.raulklahold.diagramacaodeclasse;
 
 public interface interfaceacao {
+	void tocar();
+	void pausar();
+	void selecionarMusica();
+	void ligar();
+	void atender();
+	void iniciarCorreioVoz();
+	void exibirPagina();
+	void adicionarNovaAba();
+	void atualizarPagina();
+	
 
 }
