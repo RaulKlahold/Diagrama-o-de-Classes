fa8621eadd6f1900d5770f522c03988b60e5a1dc
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-09 22:50:33 -0300

    implementação d aterceira interface

--- DiagramacaoDeClasses/src/com/github/raulklahold/diagramacaodeclasse/NavegadorDeInternet.java
+++ DiagramacaoDeClasses/src/com/github/raulklahold/diagramacaodeclasse/NavegadorDeInternet.java
@@ -1,5 +1,22 @@
 package com.github.raulklahold.diagramacaodeclasse;
 
-public class NavegadorDeInternet {
+public class NavegadorDeInternet implements InterfaceacaoTres {
+
+	@Override
+	public void exibirPagina() {
+		System.out.println("A pagina está sendo exibida!");
+	}
+
+	@Override
+	public void adicionarNovaAba() {
+		System.out.println("A pagina está adicionando uma nova aba!");
+		
+	}
+
+	@Override
+	public void atualizarPagina() {
+		System.out.println("A pagina foi atualizada!");
+		
+	}
 
 }
